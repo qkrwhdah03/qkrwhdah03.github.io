1737c19b5e44b54196540ab69d05647e473e7bce
qkrwhdah03 <qkrwhdah03@naver.com>
2020-01-02 16:51:59 +0900

    stadard of my blog

--- README.md
+++
@@ -1,3 +0,0 @@
-# qkrwhdah03.github.io
-
-<h2> This is qkrwhdah03's Blog... I'm preparing for it <h2>
--- post.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-files=fs.readdirSync("./post/");
-for(var i=0; i<files.length; i++){
-  document.write(files[i]+"<br>");
-}
