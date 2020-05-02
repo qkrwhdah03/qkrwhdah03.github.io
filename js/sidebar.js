029440525328fecd25cd50c2e78e39d97ed1910e
qkrwhdah03 <qkrwhdah03@naver.com>
2020-05-02 13:25:22 +0900

    Change Theme

--- js/sidebar.js
+++
@@ -1,9 +0,0 @@
-function opensidebar(){
-  document.getElementById('side').style.width='250px';
-  document.getElementById('side').style.paddingLeft='20px';
-}
-
-function closesidebar(){
-  document.getElementById('side').style.width='0';
-  document.getElementById('side').style.paddingLeft='0';
-}
